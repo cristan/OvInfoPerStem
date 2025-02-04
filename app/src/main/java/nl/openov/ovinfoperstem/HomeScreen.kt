e346928af4c2e21ef1c2d2544b4a19613a692032
Cristan Meijer <cristan.meijer@gmail.com>
2025-02-04 12:09:13 +0100

    Extract HomeScreen to a new file

---
+++ app/src/main/java/nl/openov/ovinfoperstem/HomeScreen.kt
@@ -0,0 +1,98 @@
+package nl.openov.ovinfoperstem
+
+import android.app.Activity
+import android.content.Intent
+import android.speech.RecognizerIntent
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import nl.openov.ovinfoperstem.ui.theme.OvInfoPerStemTheme
+import java.util.Locale
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen() {
+    OvInfoPerStemTheme {
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            topBar = {
+                TopAppBar(
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primary,
+                        actionIconContentColor = MaterialTheme.colorScheme.onPrimary,
+                        titleContentColor = MaterialTheme.colorScheme.onPrimary,
+                    ),
+                    title = {
+                        Text(stringResource(R.string.app_name))
+                    }
+                )
+            },
+            contentWindowInsets = WindowInsets(0, 0, 0, 0)
+        ) { innerPadding ->
+            val speechText = remember { mutableStateOf("Your speech will appear here.") }
+            val launcher = rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                if (it.resultCode == Activity.RESULT_OK) {
+                    val data = it.data
+                    val result = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                    speechText.value = result?.get(0) ?: "No speech detected."
+                } else {
+                    speechText.value = "[Speech recognition failed.]"
+                }
+            }
+            Column(modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Button(onClick = {
+                    val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+                    intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+                    intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+                    intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Go on then, say something.")
+                    launcher.launch(intent)
+                }) {
+                    Image(
+                        painter = painterResource(id = R.drawable.baseline_mic_24),
+                        contentDescription = null,
+                        modifier = Modifier.size(92.dp),
+                    )
+                }
+                Spacer(modifier = Modifier.padding(16.dp))
+                Text(speechText.value, textAlign = TextAlign.Center)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    OvInfoPerStemTheme {
+        HomeScreen()
+    }
+}
