3b9293f579a83c4b9e4e9d889f7386abeda72a7f
Cristan Meijer <cristan.meijer@gmail.com>
2025-02-04 11:49:56 +0100

    Add speech to text

--- app/src/main/java/nl/openov/ovinfoperstem/MainActivity.kt
+++ app/src/main/java/nl/openov/ovinfoperstem/MainActivity.kt
@@ -1,12 +1,18 @@
 package nl.openov.ovinfoperstem
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.speech.RecognizerIntent
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -21,6 +27,8 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -30,6 +38,7 @@ import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import nl.openov.ovinfoperstem.ui.theme.OvInfoPerStemTheme
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -67,20 +76,37 @@ fun HomeScreen() {
             },
             contentWindowInsets = WindowInsets(0, 0, 0, 0)
         ) { innerPadding ->
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(innerPadding),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
+            val speechText = remember { mutableStateOf("Your speech will appear here.") }
+            val launcher = rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                if (it.resultCode == Activity.RESULT_OK) {
+                    val data = it.data
+                    val result = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                    speechText.value = result?.get(0) ?: "No speech detected."
+                } else {
+                    speechText.value = "[Speech recognition failed.]"
+                }
+            }
+            Column(modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
             ) {
-                Button(onClick = {}) {
+                Button(onClick = {
+                    val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+                    intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+                    intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+                    intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Go on then, say something.")
+                    launcher.launch(intent)
+                }) {
                     Image(
                         painter = painterResource(id = R.drawable.baseline_mic_24),
                         contentDescription = null,
                         modifier = Modifier.size(92.dp),
                     )
                 }
+                Spacer(modifier = Modifier.padding(16.dp))
+                Text(speechText.value)
             }
         }
     }
