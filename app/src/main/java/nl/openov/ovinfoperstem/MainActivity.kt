3fa13a3c74f5c9e8c3263f5ca45b21db8e6aca3a
Cristan Meijer <cristan.meijer@gmail.com>
2025-02-04 11:53:34 +0100

    Center align text

--- app/src/main/java/nl/openov/ovinfoperstem/MainActivity.kt
+++ app/src/main/java/nl/openov/ovinfoperstem/MainActivity.kt
@@ -33,6 +33,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
@@ -106,7 +107,7 @@ fun HomeScreen() {
                     )
                 }
                 Spacer(modifier = Modifier.padding(16.dp))
-                Text(speechText.value)
+                Text(speechText.value, textAlign = TextAlign.Center)
             }
         }
     }
